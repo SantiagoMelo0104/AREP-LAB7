6c0803553642c9eff0f2326f3cd6e062ced75136
SantiagoMelo0104 <Santiago.melo0104@gmail.com>
2024-03-20 22:22:43 -0500

    final :3

--- src/main/java/org/arep/SecureURLReader.java
+++ src/main/java/org/arep/SecureURLReader.java
@@ -75,15 +75,40 @@ public class SecureURLReader {
     public static String readURL(String sitetoread) {
         StringBuffer response = new StringBuffer();
         try {
+            // Crea el objeto que representa una URL2
             URL siteURL = new URL(sitetoread);
+            // Crea el objeto que URLConnection
             URLConnection urlConnection = siteURL.openConnection();
+            // Obtiene los campos del encabezado y los almacena en un estructura Map
+            Map<String, List<String>> headers = urlConnection.getHeaderFields();
+            // Obtiene una vista del mapa como conjunto de pares <K,V>
+            // para poder navegarlo
+            Set<Map.Entry<String, List<String>>> entrySet = headers.entrySet();
+            // Recorre la lista de campos e imprime los valores
+            for (Map.Entry<String, List<String>> entry : entrySet) {
+                String headerName = entry.getKey();
+
+                //Si el nombre es nulo, significa que es la linea de estado
+                if (headerName != null) {
+                    System.out.print(headerName + ":");
+                }
+                List<String> headerValues = entry.getValue();
+                for (String value : headerValues) {
+                    System.out.print(value);
+                }
+                System.out.println("");
+            }
+
+            System.out.println("-------message-body------");
             BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
             String inputLine = null;
             while ((inputLine = reader.readLine()) != null) {
                 System.out.println(inputLine);
                 response.append(inputLine);
             }
         } catch (IOException x) {
+            System.err.println(x);
             response.append(x);
         }
         return response.toString();
